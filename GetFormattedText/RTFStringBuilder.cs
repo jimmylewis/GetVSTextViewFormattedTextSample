96b678d8d586226a487f9056717846e0d114dc04
Jimmy Lewis <jimmy.lewis@live.com>
2017-04-18 12:51:33 -0700

    Add RTFStringBuilder

---
+++ GetFormattedText/RTFStringBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GetFormattedText
+{
+    class RTFStringBuilder
+    {
+        RichTextBox rtb = new RichTextBox();
+
+        public void AppendText(string text, Color textColor)
+        {
+            rtb.SelectionColor = textColor;
+            rtb.AppendText(text);
+            
+        }
+
+        public override string ToString()
+        {
+            rtb.SelectAll();
+            string result = rtb.SelectedRtf;
+            rtb.Select(rtb.TextLength, 0);
+            return result;
+        }
+    }
+}
